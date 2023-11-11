ce52ec0fbc6e76525bc1d27309be367c18a150f0
dadsdasdasasww <99072317+dadsdasdasasww@users.noreply.github.com>
2023-11-11 12:24:16 +0800

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -1,4 +1,10 @@
-apt update&apt upgrade
-apt-get update && apt-get upgrade
-apt-get install wireguard
-vim /etc/wireguard/wg0.conf
+[Interface]
+PrivateKey = WBxJQiZbxhmn5m3Y867hzSUCq+swfqpjPeeVDRP1300=
+Address = 172.16.0.2/32,2606:4700:110:85a0:c06f:2a59:a75a:e9a1/128
+DNS = 1.1.1.1
+MTU = 1500
+[Peer]
+PublicKey = bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=
+AllowedIPs = 0.0.0.0/0
+AllowedIPs = ::/0
+Endpoint = 162.159.192.159:7103
